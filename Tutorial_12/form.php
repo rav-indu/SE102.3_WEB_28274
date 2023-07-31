83744ec82e6047d05a0fe2f7edfd42236b6e6d14
Ravindu Induwara <135868608+rav-indu@users.noreply.github.com>
2023-07-31 23:25:38 +0530

    Tute 12

---
+++ Tutorial_12/form.php
@@ -0,0 +1,21 @@
+<?php
+
+$fname=$_POST["fn"];
+$lname=$_POST["ln"];
+$uname=$_POST["un"];
+$password=$_POST["pw"];
+$gender=$_POST["g"];
+$year=$_POST["y"];
+$email=$_POST["em"];
+$tel=$_POST["tp"];
+
+echo "First Name: ".$fname;
+echo "Last Name: ".$lname;
+echo "Userame: ".$uname;
+echo "Password: ".$password;
+echo "Gender: ".$gender;
+echo "Academic Year: ".$year;
+echo "E-mail: ".$email;
+echo "Telephone Number: ".$tel;
+
+?>
